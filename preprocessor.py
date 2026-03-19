46df1ee664c6d5e0ed1b69883d28e9bbb0976f94
kirshtalreja24 <161924216+kirshtalreja24@users.noreply.github.com>
2026-03-19 17:12:14 +0500

    readStopWords

--- preprocessor.py
+++ preprocessor.py
@@ -1,1 +1,30 @@
 import DocumentExtraction
+import re
+import unidecode
+from nltk.tokenize import word_tokenize   #splits sentences into words
+from nltk.tokenize import sent_tokenize   #splits text into sentences
+from nltk.stem import WordNetLemmatizer   # a lemmatizer
+
+
+class InvertedIndex:    
+    def __init__(self):
+        self.words = {}
+        self.stopwords = []
+        self.lemmatizer = WordNetLemmatizer()
+        self.punctuation_table = str.maketrans('', '', r'!"#$%&()*+,-./:;<=>?@[\]^_`{|}~') #remove punctuation from text
+        
+    
+    def readStopWords(self):
+        with open('Stopword-List.txt') as file:
+            lines = file.readlines()
+            for line in lines:
+                self.stopwords.append(line.strip())
+
+
+
+
+# ob1 = InvertedIndex()
+# ob1.readStopWords()
+# print(ob1.stopwords)
+        
+        
